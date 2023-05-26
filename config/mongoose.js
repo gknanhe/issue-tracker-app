5ddb1b27cbf4eb4502d4a87a8e429a232da70b1f
gknanhe <cse18gknanhejd@gmail.com>
2023-05-26 13:32:10 +0530

    deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -33,3 +33,6 @@ db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 db.once('open',  function(){
      console.log('Connected to Database :: MongoDB');
 });
+
+
+module.exports = db;
